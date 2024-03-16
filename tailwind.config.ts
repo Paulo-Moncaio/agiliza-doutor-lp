95a32ad950f8a5aa116a423b61518e5751ab8899
Paulo-Moncaio <paulomoncaio@gmail.com>
2024-03-15 23:41:02 -0300

    adding project colors theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        agiliza: {
+          green: '#00D46F',
+          'green-strong': '#2A7F56',
+          cinza: '#E5E5E5',
+          mostarda: '#D48D00',
+          'azul-naval': '#1B1C34',
+          roxo: '#472A54',
+        },
+      },
     },
   },
   plugins: [],
