d9e899dc8b4ea812bc9d69fc0db5c78c3d48964e
Paulo-Moncaio <paulomoncaio@gmail.com>
2024-03-18 22:55:42 -0300

    section footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from '@/components/About'
 import Flow from '@/components/Flow'
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import HowItWorks from '@/components/HowItWorks'
@@ -18,6 +19,7 @@ export default function Home() {
         <Journey />
         <HowItWorks />
       </main>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import emailIcon from '../../public/email.svg'
+import cellphoneIcon from '../../public/cellphone.svg'
+import briefcaseIcon from '../../public/briefcase.svg'
+
+export default function Footer() {
+  return (
+    <footer className="max-w-5xl px-4 mx-auto">
+      <div className="flex justify-between lg:justify-around flex-col md:flex-row mt-10 mb-4 text-sm gap-y-8">
+        <div>
+          <Image src={'/agiliza-logo.svg'} width={118} height={51} alt={''} />
+        </div>
+        <div className="flex flex-col">
+          <p>agiliza doutor</p>
+          <div className="text-gray-500 font-normal mt-2 flex flex-col">
+            <Link href={'#'}>Quem somos</Link>
+            <Link href={'#'}>Como funciona</Link>
+            <Link href={'#'}>Soluções</Link>
+            <Link href={'#'}>Blog</Link>
+            <Link href={'#'}>Solicitar demo</Link>
+          </div>
+        </div>
+        <div className="space-y-8">
+          <Link
+            href={'#'}
+            className="flex bg-agiliza-green py-4 text-center justify-center"
+          >
+            <Image src={briefcaseIcon} alt={''} />
+            <p className="pl-4">solicite sua proposta</p>
+          </Link>
+          <div className="flex flex-col gap-y-1">
+            <p className="mb-1">contato</p>
+            <div className="flex gap-x-2">
+              <Image src={emailIcon} alt={''} />
+              <p className="text-gray-500 font-normal">
+                atendimento@agilizadoutor.com.br
+              </p>
+            </div>
+            <div className="flex">
+              <Image src={cellphoneIcon} alt={''} />
+              <p className="text-gray-500 font-normal">+55 (61) 99605 1101</p>
+            </div>
+          </div>
+          <div>
+            <p>siga nossas redes sociais</p>
+          </div>
+        </div>
+      </div>
+      <span>© 2024 AgilizaDoutor. Todos os direitos reservados.</span>
+    </footer>
+  )
+}
