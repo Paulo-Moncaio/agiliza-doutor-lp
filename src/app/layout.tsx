9d802da28bbf0f73aa519408f21e39304401dff9
Paulo-Moncaio <paulomoncaio@gmail.com>
2024-03-26 14:06:37 -0300

    adding Vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={(roboto.className, 'w-full  mx-auto')}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
